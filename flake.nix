50bf3f0900166117e1d81dcbde8e31270d15f8a4
Matt Burdan <burdz@burdz.net>
2021-09-23 09:34:55 -0700

    play around with flakes and buildInputs

--- flake.nix
+++ flake.nix
@@ -1,16 +1,24 @@
 {
   description = "Automatic GitHub Gist uploads for selections in URxvt";
 
-  outputs = { self, nixpkgs, }: {
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-
-      stdenv.mkDerivation {
-        name = "urxvt-gist";
-        src = self;
-        installPhase = ''
-          mkdir -p $out/lib/urxvt/perl; install -t $out/lib/urxvt/perl urxvt-gist
-        '';
+  outputs = { self, nixpkgs, }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
       };
-  };
+
+      perlPackages = pkgs.perlPackages;
+    in {
+      defaultPackage.x86_64-linux =
+        pkgs.stdenv.mkDerivation {
+          name = "urxvt-gist";
+          src = self;
+          buildInputs = with perlPackages; [
+            JSONMaybeXS
+          ];
+          installPhase = ''
+            mkdir -p $out/lib/urxvt/perl; install -t $out/lib/urxvt/perl urxvt-gist
+          '';
+        };
+    };
 }
